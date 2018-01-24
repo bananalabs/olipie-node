c2c585b960c506564dc50a263ff9c4b6829a0b47
dramam <dshenoy@gmail.com>
2018-01-24 10:22:39 -0800

    add hook for update

--- src/services/video/video.hooks.js
+++ src/services/video/video.hooks.js
@@ -8,22 +8,29 @@ module.exports = {
     find: [
       async function(context) {
         const userId = context.params.query['user'];
+        if (!userId) {
+          return context;
+        }
         const uservideos = await context.app.service('uservideo').find({
           query: {
             userId: userId
           }
         });
         const videoIds= uservideos.data.map((uservideo) => uservideo.videoId);
-        context.params.query = {
-          id: {
-            [Op.in]: videoIds
-          }
+        delete context.params.query['user'];
+        context.params.query['id'] = {
+          [Op.in]: videoIds
         };
         return context;
       }
     ],
     create: [],
-    update: [],
+    update: [
+      async function(context) {
+        context.id = context.data.id;
+        return context;
+      }
+    ],
     patch: [],
     remove: []
   },
